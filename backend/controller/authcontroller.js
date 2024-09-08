4559d8835793cce04c4a7d1db112892705004bfe
BOT22345 <nmongia48@gmail.com>
2024-09-08 20:17:40 +0530

    created the login endpoint

--- backend/controller/authcontroller.js
+++ backend/controller/authcontroller.js
@@ -2,9 +2,7 @@ import User from "../models/usermodel.js";
 import bcrypt from "bcryptjs"
 import generateTokenandSetCookie from "../utils/generateToken.js";
 
-export const loginUser=(req,res)=>{
-    res.send("login user");
-}
+
 export const  signupUser=async (req,res)=>{
     try{
         const {fullname,username,password,confirmPassword,gender}=req.body;
@@ -56,6 +54,32 @@ export const  signupUser=async (req,res)=>{
         res.status(500).json({error:"internal server error "})
     }
 }
+
+export const loginUser=async (req,res)=>{
+ try {
+    
+    const {username,password}=req.body; 
+    const user=await User.findOne({username});
+    const isPasswordCorrect=await bcrypt.compare(password, user?.password || "");
+    
+    if(!user || !isPasswordCorrect){
+        return res.status(400).json({error:"Invalid username or password"});
+    }
+
+    generateTokenandSetCookie(user.id,res);
+    res.status(200).json({
+        _id:user._id,
+        fullname:user.fullname,
+        username:user.username,
+        profilepic:user.profilepic
+    })
+
+ } catch (error) {
+    console.log("Error in login controller",error.message);
+    res.status(500).json({error:"internal server error "})
+ }
+}
+
 export const logoutUser=(req,res)=>{
     res.send("logout user");
 }
