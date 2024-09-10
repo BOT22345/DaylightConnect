7135422fec013775f99e9fd2e3a726831f28c7f6
BOT22345 <nmongia48@gmail.com>
2024-09-10 10:41:52 +0530

    added the get messages endpoint for a particular user

--- backend/controller/messagecontroller.js
+++ backend/controller/messagecontroller.js
@@ -40,3 +40,21 @@ export const sendMessage=async (req,res)=>{
         res.status(500).json({error:"Internal server error"});
     }
 }
+
+export const getMessages=async (req,res)=>{
+    try {
+        const {id:userToChatId}=req.params;
+        const senderId=req.user._id;
+
+        const conversation=await Conversation.findOne({
+            participants:{$all : [senderId,userToChatId]},
+
+        }).populate("messages");
+
+        res.status(200).json(conversation.messages)
+
+    } catch (error) {
+        console.log("Error in get message controller",error.message);
+        res.status(500).json({error:"Internal server error"});
+    }
+}
--- backend/routes/messageroutes.js
+++ backend/routes/messageroutes.js
@@ -1,9 +1,10 @@
 import express from "express"
-import {sendMessage} from "../controller/messagecontroller.js"
+import {getMessages, sendMessage} from "../controller/messagecontroller.js"
 import protectRoute from "../middleware/protectRouter.js";
 
 const router=express.Router();
 
+router.get("/:id",protectRoute,getMessages)
 router.post("/send/:id",protectRoute,sendMessage)
 
 export default router;
