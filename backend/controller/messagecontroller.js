959a9a91a4e6aef7ae2d7c1e1dd8a969d7869ffc
BOT22345 <nmongia48@gmail.com>
2024-09-11 11:02:40 +0530

    added the tailwind css and daisy ui for tailwind css

--- backend/controller/messagecontroller.js
+++ backend/controller/messagecontroller.js
@@ -51,7 +51,14 @@ export const getMessages=async (req,res)=>{
 
         }).populate("messages");
 
-        res.status(200).json(conversation.messages)
+        if(!conversation){
+           return res.status(200).json([]);
+        }
+
+        const messages=conversation.messages;
+        
+        res.status(200).json(messages)
+
 
     } catch (error) {
         console.log("Error in get message controller",error.message);
