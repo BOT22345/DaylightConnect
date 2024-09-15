5f39927b183393254d93fdfbad801577654e33e0
BOT22345 <nmongia48@gmail.com>
2024-09-15 11:44:18 +0530

    added the whole in browser router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,4 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/home/Home'
 import Login from './pages/login/login'
@@ -8,9 +9,11 @@ function App() {
   return (
     <>
     <div className="p-4 h-screen flex items-center justify-center">
-    {/* <Login></Login> */}
-    {/* <SignUp></SignUp> */}
-    <Home></Home>
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/login' element={<Login></Login>}/>
+      <Route path="/signup" element={<SignUp></SignUp>}/>
+    </Routes>
     </div>
     </>
   )
