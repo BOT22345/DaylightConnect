22c21682416444c8d48c316aa27def293212fa1d
BOT22345 <nmongia48@gmail.com>
2024-09-17 18:08:53 +0530

    added the logic when you sign up it will navigate you to home page using use context and create context hook

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,19 +1,23 @@
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/home/Home'
 import Login from './pages/login/login'
 import {Toaster} from 'react-hot-toast'
 import SignUp from './pages/signup/Signup'
+import { useAuthContext } from './context/AuthContext.jsx'
 
 function App() {
-  
+  const {authUser} =useAuthContext();
+  console.log(authUser);
   return (
     <>
     <div className="p-4 h-screen flex items-center justify-center">
     <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/login' element={<Login></Login>}/>
-      <Route path="/signup" element={<SignUp></SignUp>}/>
+
+      <Route path='/' element={<Home/>}></Route>
+      {/* <Route path='/login' element={<Login/>}></Route> */}
+      <Route path='/login' element={authUser?<Navigate to="/"/>:<Login></Login>}/>
+      <Route path="/signup" element={authUser ? <Navigate to="/" /> : <SignUp></SignUp>}/>
     </Routes>
     <Toaster></Toaster>
     </div>
--- frontend/src/hooks/useSignup.js
+++ frontend/src/hooks/useSignup.js
@@ -1,49 +1,59 @@
-import { useState } from "react"
-import toast from "react-hot-toast";
+    import { useState } from "react"
+    import toast from "react-hot-toast";
+    import { useAuthContext } from "../context/AuthContext";
 
-const useSignup = () => {
-    const [loading,setLoading]=useState(false);
-    
-    const signup=async({fullname,username,password,confirmPassword,gender})=>{
-        const success= handleInputErrors({fullname,username,password,confirmPassword,gender})
-        if(!success)return;
+    const useSignup = () => {
+        const [loading,setLoading]=useState(false);
+        const {setAuthUser}=useAuthContext()
+        
 
-        try{
+        const signup=async({fullname,username,password,confirmPassword,gender})=>{
+            const success= handleInputErrors({fullname,username,password,confirmPassword,gender})
+            if(!success)return;
 
-            const res=await fetch("/api/auth/signup",{
-                method:"POST",
-                headers:{"Content-Type":"application/json"},
-                body:JSON.stringify({fullname,username,password,confirmPassword,gender})
-            })
+            try{
 
-            const data =await res.json();
-            console.log(data)
-        }catch(error){
-            toast.error(error.message)
-        }finally{
-            setLoading(false);
+                const res=await fetch("/api/auth/signup",{
+                    method:"POST",
+                    headers:{"Content-Type":"application/json"},
+                    body:JSON.stringify({fullname,username,password,confirmPassword,gender})
+                })
+
+                const data =await res.json();
+                if(data.error){
+                    throw new Error(data.error)
+                }
+
+
+                localStorage.setItem("chat-user",JSON.stringify(data))
+
+                setAuthUser(data)
+            }catch(error){
+                toast.error(error.message)
+            }finally{
+                setLoading(false);
+            }
         }
-    }
-    return {loading,signup}
-}
-
-export default useSignup
-
-function handleInputErrors({fullname,username,password,confirmPassword,gender}){
-    if(!fullname || !username || !password || !confirmPassword || !gender){
-        toast.error('please fill all the fields')
-        return false;
-    }
-    
-    if(password !==confirmPassword){
-        toast.error("incorrect confirm password")
-        return false;
+        return {loading,signup}
     }
 
-    if(password.length <6){
-        toast.error("password must be atleast 6 characters")
-        return false;
+    export default useSignup
+
+    function handleInputErrors({fullname,username,password,confirmPassword,gender}){
+        if(!fullname || !username || !password || !confirmPassword || !gender){
+            toast.error('please fill all the fields')
+            return false;
+        }
+        
+        if(password !==confirmPassword){
+            toast.error("incorrect confirm password")
+            return false;
+        }
+
+        if(password.length <6){
+            toast.error("password must be atleast 6 characters")
+            return false;
+        }
+        alert("successfull sign up")
+        return true;
     }
-    alert("successfull sign up")
-    return true;
-}
