ccc3a5cf593ecab8079ceaa93595b83f733e0bab
BOT22345 <nmongia48@gmail.com>
2024-09-18 10:18:39 +0530

    added the logout button functionality

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,8 +14,8 @@ function App() {
     <div className="p-4 h-screen flex items-center justify-center">
     <Routes>
 
-      <Route path='/' element={<Home/>}></Route>
-      {/* <Route path='/login' element={<Login/>}></Route> */}
+      {/* <Route path='/' element={<Home/>}></Route> */}
+      <Route path='/' element={authUser?<Home></Home>: <Navigate to={"/login"}></Navigate>}></Route>
       <Route path='/login' element={authUser?<Navigate to="/"/>:<Login></Login>}/>
       <Route path="/signup" element={authUser ? <Navigate to="/" /> : <SignUp></SignUp>}/>
     </Routes>
--- frontend/src/components/sidebar/LogoutButton.jsx
+++ frontend/src/components/sidebar/LogoutButton.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import { BiLogOut } from "react-icons/bi";
+import useLogout from '../../hooks/useLogout';
 
 
 const LogoutButton = () => {
+
+  const { loading, logout } = useLogout()
+
   return (
     <div className='mt-auto'>
-        <BiLogOut className='h-6 w-6 text-white cursor-pointer'></BiLogOut>
+      {!loading ? (
+        <BiLogOut className='h-6 w-6 text-white cursor-pointer'
+          onClick={logout}
+        ></BiLogOut>
+      ) : (
+        <span className='loading loading-spinner'></span>
+      )}
     </div>
   )
 }
