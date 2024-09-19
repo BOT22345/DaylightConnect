8ee35b22409a792a248e7d08676b72471455ada0
BOT22345 <nmongia48@gmail.com>
2024-09-19 08:58:35 +0530

    added the logic where you click the conversation and it will be highlighted

--- frontend/src/components/sidebar/Conversation.jsx
+++ frontend/src/components/sidebar/Conversation.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
+import useConversation from '../../zustand/useConversation'
+
+
 
 const Conversation = ({conversation,lastIdx}) => {
-  return (
+
+    const {selectedConversation,setSelectedConversation} =useConversation()
+     
+    const isSelected=selectedConversation?._id ===conversation._id;
+  
+    return (
     <>
-        <div className='flex gap-2 items:center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer'> 
+        <div className={`flex gap-2 items:center hover:bg-sky-500 rounded p-2 py-1 cursor-pointer
+                ${isSelected ? "bg-sky-500": ""} 
+            `}
+            onClick={()=>{setSelectedConversation(conversation)}}
+            > 
             <div className='avatar online '>
                 <div className='w-12 rounded-full'> 
                     <img src={conversation.profilepic}alt="user avatar"></img>
