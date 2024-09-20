dc9c2203d6137bbd5517b7b6b6e892538d448c87
BOT22345 <nmongia48@gmail.com>
2024-09-20 10:57:47 +0530

    added the hook to get all messages from the backend

--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
 import Message from './Message'
+import useGetMessages from '../../hooks/useGetMessages'
 
 const Messages = () => {
+  const {messages,loading } =useGetMessages()
   return (
     <div className='px-4 flex-1 overflow-auto '>
         <Message></Message>
