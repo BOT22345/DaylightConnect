b6b626cc418ec43abda773be0698b29bd4798fb1
BOT22345 <nmongia48@gmail.com>
2024-09-20 10:21:11 +0530

    added the send message functionality by fetching the send message api form backend

--- frontend/src/components/messages/MessageContainer.jsx
+++ frontend/src/components/messages/MessageContainer.jsx
@@ -1,17 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Messages from './Messages'
 import MessageInput from './MessageInput'
 import { TiMessages } from 'react-icons/ti'
+import useConversation from '../../zustand/useConversation'
 
 const MessageContainer = () => {
-    const nochatselected=true;
+   const {selectedConversation,setSelectedConversation}= useConversation()
+
+   useEffect(()=>{
+    return ()=>{
+        setSelectedConversation(null)
+    }
+   },[setSelectedConversation]);
   return (
     <div className='md:min-w-[450px] flex flex-col'>
-        {nochatselected ? <NoChatSelected></NoChatSelected> : (
+        {!selectedConversation ? <NoChatSelected></NoChatSelected> : (
             <>
             <div className='bg-slate-500 px-4 oy-2 mb-2'>
                 <span className='label-text'>To:</span>{" "}
-                <span className='text-indigo-900 font-bold'>Nayan</span>
+                <span className='text-indigo-900 font-bold'>{selectedConversation.fullname}</span>
             </div>
             
             <Messages></Messages>
