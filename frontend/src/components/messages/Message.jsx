5dfb894baabcd23cc073fc9614e0ce1efaad1102
BOT22345 <nmongia48@gmail.com>
2024-09-21 12:49:25 +0530

    added the logic for sent message and recieved message to be displayed on the screen

--- frontend/src/components/messages/Message.jsx
+++ frontend/src/components/messages/Message.jsx
@@ -1,18 +1,29 @@
 import React from 'react'
+import {useAuthContext} from "../../context/AuthContext.jsx"
+import useConversation from '../../zustand/useConversation.js';
 
-const Message = () => {
+const Message = ({message}) => {
+  const {authUser} = useAuthContext();
+  const {selectedConversation}=useConversation()
+  const fromMe=message.senderId===authUser._id; 
+  const chatClassname =fromMe?'chat-end':'chat-start'
+  const profilePics=fromMe ? authUser.profilepic : selectedConversation?.profilepic
+  console.log("Auth User Profile Pic:", authUser.profilepic);
+console.log("Selected Conversation Profile Pic:", selectedConversation?.profilePic);
+
+  const bubbleBgColor=fromMe ? 'bg-blue-500': ""
   return (
-    <div className='chat chat-end '>
+    <div className={`chat ${chatClassname}`}>
         <div className='chat-image avatar'> 
             <div className='w-10 rounded-full'>
                 <img
                 alt="message container chat icon"
-                src="https://avatar.iran.liara.run/public"
+                src={profilePics}
                 />
             </div>
         </div>
-        <div className={`chat-bubble text-white bg-blue-500`}>Hi! what's up</div>
-        <div className={`chat-footer opacity-100 text-xs flex gap-1 items-center text-cyan-400`}>12:00</div>
+        <div className={`chat-bubble text-white ${bubbleBgColor}`}>{message.message}</div>
+        <div className={`chat-footer opacity-100 text-xs flex gap-1 items-center text-cyan-400`}>{message.createdAt}</div>
     </div>
   )
 }
--- frontend/src/components/messages/Messages.jsx
+++ frontend/src/components/messages/Messages.jsx
@@ -1,20 +1,21 @@
-import React from 'react'
-import Message from './Message'
+import Message from "./Message"
 import useGetMessages from '../../hooks/useGetMessages'
+import MessageSkeleton from '../skeletons/MessageSkeleton'
 
 const Messages = () => {
-  const {messages,loading } =useGetMessages()
+  const { messages, loading } = useGetMessages()
   return (
     <div className='px-4 flex-1 overflow-auto '>
-        <Message></Message>
-        <Message></Message>
-        <Message></Message>
-        <Message></Message>
-        <Message></Message>
-        <Message></Message>
-        <Message></Message>
-        <Message></Message>
+        
+      {!loading && messages.length > 0 && messages.map((message) => (
+        <Message key={message._id} message={message}/>
+      ))}
 
+      {loading && [...Array(3)].map((_, idx) => <MessageSkeleton key={idx}></MessageSkeleton>)}
+
+      {!loading && messages.length === 0 && (
+        <p className='text-center text-white'>Send a message to start the conversation</p>
+      )}
     </div>
   )
 }
