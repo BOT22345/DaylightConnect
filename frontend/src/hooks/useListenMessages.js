3538fc68b5d149e54a9c317d34baeef73f32be18
BOT22345 <nmongia48@gmail.com>
2024-09-23 14:24:33 +0530

    added the notifcation sound on message recieving

--- frontend/src/hooks/useListenMessages.js
+++ frontend/src/hooks/useListenMessages.js
@@ -3,12 +3,16 @@ import {useEffect} from 'react'
 import {useSocketContext} from  "../context/SocketContext"
 import useConversation from "../zustand/useConversation"
 
+import notification from "../assets/sounds/notification.mp3"
+
 const useListenMessages = () => {
-    const {socket}= useSocketContext()
+    const {socket}= useSocketContext() 
     const {messages,setMessages}=useConversation() 
 
     useEffect(()=>{
         socket?.on("newMessage",(newMessage)=>{
+            const sound=new Audio(notification)
+            sound.play();
             setMessages([...messages,newMessage])
         })
 
