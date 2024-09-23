4fdfe6e689c0baa9cef417d78dc0a3061e9f4c68
BOT22345 <nmongia48@gmail.com>
2024-09-23 15:57:56 +0530

    made the necessary changes for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+import path from 'path'
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -11,6 +12,8 @@ import { app,server,io } from "./socket/socket.js";
 
 const PORT =process.env.PORT || 5000;
 
+const __dirname=path.resolve();
+
 dotenv.config();
 
 app.use(express.json()); // to parse the incoming requrests with json 
@@ -19,6 +22,12 @@ app.use(cookieParser());
 app.use('/api/auth',authRoutes);
 app.use('/api/messages',messageRoutes);
 app.use('/api/users',userRoutes);
+
+app.use(express.static(path.join(__dirname,"frontend/dist")))
+
+app.get("*",(req,res)=>{
+    res.sendFile(path.join(__dirname,"frontend","dist","index.html"))
+})
 
 // app.get('/',(req,res)=>{
 //     res.send("chat app backend");
