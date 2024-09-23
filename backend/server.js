a3d9b8b7ab1d52227f0b682d5a9862cf546a1101
BOT22345 <nmongia48@gmail.com>
2024-09-23 12:45:52 +0530

    successfully reieveing the user logedin id at backend

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,8 @@ import messageRoutes from "./routes/messageroutes.js";
 import userRoutes from "./routes/userroutes.js";
 
 import connectToMongoDB from "./db/connectToMongoDB.js";
+import { app,server,io } from "./socket/socket.js";
 
-const app=express();
 const PORT =process.env.PORT || 5000;
 
 dotenv.config();
@@ -24,7 +24,7 @@ app.use('/api/users',userRoutes);
 //     res.send("chat app backend");
 // });
 
-app.listen(PORT,()=>{
+server.listen(PORT,()=>{
     connectToMongoDB();
     console.log(`Server is running on the port ${PORT}`);
 });
