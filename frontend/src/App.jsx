8abe6bcec16adbc0426aacbc35d7c4d20dad4271
BOT22345 <nmongia48@gmail.com>
2024-09-23 16:14:28 +0530

    again fixed some more login component changes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/home/Home'
-import Login from './pages/login/login'
+import Login from './pages/login/Login.jsx'
 import {Toaster} from 'react-hot-toast'
 import SignUp from './pages/signup/Signup'
 import { useAuthContext } from './context/AuthContext.jsx'
