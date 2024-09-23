39d8e233a73c97b6189351c52659c43f6e01194d
BOT22345 <nmongia48@gmail.com>
2024-09-23 16:09:04 +0530

    error in login while deployement

--- frontend/src/pages/login/Login.jsx
+++ frontend/src/pages/login/Login.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react'
 import {Link} from "react-router-dom"
-import useLogin from '../../hooks/useLogin';
+import useLogin from '../../hooks/useLogin.js';
 
 const Login = () => {
 
